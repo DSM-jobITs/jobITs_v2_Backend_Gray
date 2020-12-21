ec7f4391e8dfdc91d3ca3238a88c52830f924ef8
yangseungbin <tanja1206@naver.com>
2020-12-21 15:35:07 +0900

    [ADD] express loader

---
+++ src/loaders/express.ts
@@ -0,0 +1,20 @@
+import * as express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { ApiNotFoundError } from "../exception";
+
+export default (app: express.Application) => {
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(morgan("dev"));
+  app.use((req, res, next) => {
+    next(ApiNotFoundError);
+  });
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+    });
+  });
+};
