4b3f89e6311507564e9667bf815fc04d1906e644
yangseungbin <tanja1206@naver.com>
2020-12-21 17:08:33 +0900

    [MODIFY] import path @

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import "module-alias/register";
 import "reflect-metadata";
 import express from "express";
 
-import loader from "./loaders";
-import config from "./config";
-import logger from "./loaders/logger";
+import loader from "@/loaders";
+import config from "@/config";
+import logger from "@/loaders/logger";
 
 async function startServer() {
   const app = express();
