c8172d2a8fa7ad09919a57ed09bc1693c1e39e07
yangseungbin <tanja1206@naver.com>
2020-12-22 15:54:03 +0900

    [MODIFY] synchronize true

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,17 @@
 import config from "./src/config";
-import { Depart } from "./src/models";
+import { Depart } from "./src/entities";
+import { ConnectionOptions } from "typeorm";
 
-export = {
+const connectionOptions: ConnectionOptions = {
   type: "mysql",
   host: config.mysql.host,
   port: 3306,
   username: config.mysql.username,
   password: config.mysql.password,
   database: config.mysql.name,
-  synchronize: false,
+  synchronize: true,
   logging: true,
   entities: [Depart],
 };
+
+export default connectionOptions;
