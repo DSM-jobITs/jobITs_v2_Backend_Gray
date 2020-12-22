ebe99203a01ae75df2e162c4905d8c3a700a420d
yangseungbin <tanja1206@naver.com>
2020-12-22 15:54:27 +0900

    [MODIFY] create connection parameter connection options

--- src/loaders/connection.ts
+++ src/loaders/connection.ts
@@ -1,9 +1,10 @@
 import { createConnection } from "typeorm";
 import logger from "@/loaders/logger";
+import connectionOptions from "@/../ormconfig";
 
 export default async () => {
   try {
-    await createConnection();
+    await createConnection(connectionOptions);
   } catch (e) {
     logger.error(`mysql connection error : ${e.message}`);
     process.exit(1);
