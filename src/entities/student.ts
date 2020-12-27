f0b6a5b53c190f5c4d4f7e8deea35220aad5424f
yangseungbin <tanja1206@naver.com>
2020-12-27 21:33:46 +0900

    [ADD] extends validation entity

--- src/entities/student.ts
+++ src/entities/student.ts
@@ -1,18 +1,24 @@
 import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn } from "typeorm";
 import { Depart } from "./depart";
+import { ValidationEntity } from "./validation";
+import { IsNotEmpty, Length } from "class-validator";
 
 @Entity()
-export class Student {
+export class Student extends ValidationEntity {
   @PrimaryColumn({ name: "std_no", type: "char", length: 4 })
   stdNo: string;
 
   @Column({ nullable: false, length: 15 })
+  @Length(6)
+  @IsNotEmpty()
   id: string;
 
   @Column({ nullable: false, length: 50 })
+  @IsNotEmpty()
   password: string;
 
   @Column({ nullable: false, length: 5 })
+  @IsNotEmpty()
   name: string;
 
   @OneToOne((type) => Depart, (depart) => depart.id)
