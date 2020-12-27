a30272eff3657a6a21807cac035a65e4e4f0b1d7
yangseungbin <tanja1206@naver.com>
2020-12-27 21:33:32 +0900

    [ADD] extends validation entity

--- src/entities/depart.ts
+++ src/entities/depart.ts
@@ -1,10 +1,15 @@
 import { Entity, PrimaryColumn, Column } from "typeorm";
+import { ValidationEntity } from "./validation";
+import { IsNotEmpty, Max, Min } from "class-validator";
 
 @Entity()
-export class Depart {
+export class Depart extends ValidationEntity {
   @PrimaryColumn()
+  @Min(1)
+  @Max(4)
   id: number;
 
-  @Column({ length: 20 })
+  @Column({ nullable: false, length: 20 })
+  @IsNotEmpty()
   dept: string;
 }
