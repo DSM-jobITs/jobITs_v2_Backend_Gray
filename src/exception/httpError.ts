fd4ed94015b130041149a78ac481a4712f44256a
yangseungbin <tanja1206@naver.com>
2020-12-28 10:03:35 +0900

    [MODIFY] error code

--- src/exception/httpError.ts
+++ src/exception/httpError.ts
@@ -1,8 +1,10 @@
 export default class HttpError extends Error {
   public status: number;
   public message: string;
-  constructor(status: number, message: string) {
+  public code: number;
+  constructor(status: number, message: string, code: number) {
     super(message);
     this.status = status;
+    this.code = code;
   }
 }
