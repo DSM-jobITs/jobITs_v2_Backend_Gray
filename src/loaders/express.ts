74990349bf4e6207df51b8cbad4270886fef2466
yangseungbin <tanja1206@naver.com>
2020-12-28 10:03:19 +0900

    [MODIFY] error code

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,20 +1,21 @@
 import * as express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import { ApiNotFoundError } from "@/exception";
+import { ApiNotFound } from "@/exception";
 
 export default (app: express.Application) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(morgan("dev"));
+
   app.use((req, res, next) => {
-    next(ApiNotFoundError);
+    next(ApiNotFound);
   });
+
   app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-    });
+    res
+      .status(err.status || 500)
+      .json({ message: err.message, code: err.code });
   });
 };
