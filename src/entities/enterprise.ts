6152ab795fed14da65832448b230d9b68396bd57
yangseungbin <tanja1206@naver.com>
2020-12-30 23:03:08 +0900

    [ADD] enterprise entity

--- src/entities/enterprise.ts
+++ src/entities/enterprise.ts
@@ -3,9 +3,24 @@ import { ValidationEntity } from "./validation";
 
 @Entity()
 export class Enterprise extends ValidationEntity {
-  @PrimaryColumn({ length: 50 })
-  id: string;
+  @PrimaryColumn({ length: 12, type: "char", name: "ent_no" })
+  entNo: string;
 
-  @Column({ length: 30 })
+  @Column({ length: 30, nullable: false })
   name: string;
+
+  @Column({ length: 12, type: "char", nullable: false })
+  phone: string;
+
+  @Column({ length: 15, name: "establishment_date", nullable: false })
+  establishmentDate: string;
+
+  @Column({ type: "float" })
+  sales: number;
+
+  @Column({ length: 255, nullable: false })
+  introduce: string;
+
+  @Column({ type: "tinyint", nullable: false })
+  sector: boolean;
 }
