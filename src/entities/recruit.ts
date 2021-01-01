8faf8d3020d2ac1ee25ce8a15116b2e45c253db9
yangseungbin <tanja1206@naver.com>
2021-01-01 23:09:06 +0900

    [ADD] recruit entity

---
+++ src/entities/recruit.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn } from "typeorm";
+import { ValidationEntity } from "./validation";
+import { Enterprise } from "./enterprise";
+
+@Entity()
+export class Recruit extends ValidationEntity {
+  @PrimaryColumn({ length: 30 })
+  id: string;
+
+  @Column({ length: 12, type: "char", nullable: false })
+  reception: string;
+
+  @Column({ length: 12, type: "char", nullable: false })
+  deadline: string;
+
+  @Column({ type: "tinyint", nullable: false, name: "recruit_plan" })
+  recruitPlan: boolean;
+
+  @OneToOne((type) => Enterprise, (enterprise) => enterprise.entNo)
+  @JoinColumn({ name: "ent_no" })
+  enterprise: Enterprise;
+}
