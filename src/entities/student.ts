1785507d024fc88ce0986f58e328bd80d313ae12
yangseungbin <tanja1206@naver.com>
2021-01-02 16:54:46 +0900

    [MODIFY] foreign key cascade

--- src/entities/student.ts
+++ src/entities/student.ts
@@ -8,11 +8,21 @@ export class Student extends ValidationEntity {
   @PrimaryColumn({ name: "std_no", type: "char", length: 4 })
   stdNo: string;
 
-  @OneToOne((type) => Depart, (depart) => depart.id)
+  @OneToOne((type) => Depart, (depart) => depart.id, {
+    cascade: true,
+    eager: true,
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
   @JoinColumn({ name: "depart_id" })
   depart: Depart;
 
-  @OneToOne((type) => User, (user) => user.id)
+  @OneToOne((type) => User, (user) => user.id, {
+    cascade: true,
+    eager: true,
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
   @JoinColumn()
   user: User;
 }
