d9680c538dcc693449eb7687b94b77e4030e3f5d
yangseungbin <tanja1206@naver.com>
2021-01-04 16:01:35 +0900

    [ADD] meal entity

---
+++ src/entities/meal.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn } from "typeorm";
+import { ValidationEntity } from "./validation";
+import { Enterprise } from "./enterprise";
+
+@Entity()
+export class Meal extends ValidationEntity {
+  @PrimaryColumn({ length: 12, type: "char", name: "ent_no" })
+  entNo: string;
+
+  @Column({ type: "tinyint", default: false })
+  breakfast: boolean;
+
+  @Column({ type: "tinyint", default: false })
+  lunch: boolean;
+
+  @Column({ type: "tinyint", default: false })
+  dinner: boolean;
+
+  @Column({ type: "tinyint", default: false })
+  salary: boolean;
+
+  @OneToOne((type) => Enterprise, (enterprise) => enterprise.entNo, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "ent_no" })
+  enterprise: Enterprise;
+}
