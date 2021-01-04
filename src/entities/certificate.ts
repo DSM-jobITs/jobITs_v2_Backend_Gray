187243fab80ef4b95c4beb5e92aac2e19c1d9bd7
yangseungbin <tanja1206@naver.com>
2021-01-04 15:14:36 +0900

    [ADD] certificate entity

---
+++ src/entities/certificate.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { Qualification } from "./qualification";
+import { ValidationEntity } from "./validation";
+
+@Entity()
+export class Certificate extends ValidationEntity {
+  @PrimaryColumn({ length: 30 })
+  id: string;
+
+  @Column({ length: 20 })
+  certificate: string;
+
+  @ManyToOne(
+    (type) => Qualification,
+    (qualification) => qualification.certficates,
+    {
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    }
+  )
+  @JoinColumn({ name: "qualification_id" })
+  qualification!: Qualification;
+}
