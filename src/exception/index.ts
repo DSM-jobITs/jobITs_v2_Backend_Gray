68613b435b2399ede454c4d4fe10f13f5890c14f
yangseungbin <tanja1206@naver.com>
2021-01-05 14:34:02 +0900

    [ADD] is not admin exception

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -2,3 +2,4 @@ import HttpError from "./httpError";
 
 export const BadRequest = new HttpError(400, "Bad Request", 1000);
 export const ApiNotFound = new HttpError(404, "Api Not Found", 1000);
+export const IsNotAdmin = new HttpError(409, "This User is Not Admin", 1000);
