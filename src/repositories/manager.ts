217c8b836ebad2aba229c361c0fd1fc840d9df86
yangseungbin <tanja1206@naver.com>
2021-01-06 14:47:11 +0900

    [MODIFY] parameter

--- src/repositories/manager.ts
+++ src/repositories/manager.ts
@@ -3,12 +3,17 @@ import { writeFirstRecruitRequest } from "@/interfaces";
 import { getRepository } from "typeorm";
 
 export class ManagerRepository {
-  public static async createManager(req: writeFirstRecruitRequest) {
+  public static async createManager(
+    entNo: string,
+    managerRank: string,
+    managerPhone: string,
+    managerEmail: string
+  ) {
     const manager = new Manager();
-    manager.entNo = req.entNo;
-    manager.rank = req.managerRank;
-    manager.phone = req.managerPhone;
-    manager.email = req.managerEmail;
+    manager.entNo = entNo;
+    manager.rank = managerRank;
+    manager.phone = managerPhone;
+    manager.email = managerEmail;
     await getRepository(Manager).save(manager);
   }
 }
