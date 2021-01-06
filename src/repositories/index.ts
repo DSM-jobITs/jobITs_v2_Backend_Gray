fd5e586f9ff84501f61beadd1ea2e2f98b9fc92b
yangseungbin <tanja1206@naver.com>
2021-01-06 16:21:22 +0900

    [ADD] export repositories

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,3 +2,7 @@ export * from "./enterprise";
 export * from "./recruit";
 export * from "./user";
 export * from "./manager";
+export * from "./certificate";
+export * from "./specialty";
+export * from "./qualification";
+export * from "./meal";
