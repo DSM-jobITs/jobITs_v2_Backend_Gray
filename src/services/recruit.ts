cea4d02908f7aaf8613ba846913f565dfaa81fdb
yangseungbin <tanja1206@naver.com>
2021-01-06 10:51:35 +0900

    [MODIFY] create recruit

--- src/services/recruit.ts
+++ src/services/recruit.ts
@@ -3,6 +3,9 @@ import { RecruitRepository } from "@/repositories";
 export class RecruitService {
   public static async getWritingRecruitId(): Promise<string> {
     const recruit = await RecruitRepository.getWritingRecruit();
+    if (!recruit) {
+      return (await RecruitRepository.createRecruit()).id;
+    }
     return recruit.id;
   }
 }
