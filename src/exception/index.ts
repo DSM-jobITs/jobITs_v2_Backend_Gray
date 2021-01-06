c9e1bdbe88115ed1112896c8aadd06c1a031427d
yangseungbin <tanja1206@naver.com>
2021-01-06 14:45:42 +0900

    [ADD] recruit not found

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -3,4 +3,5 @@ import HttpError from "./httpError";
 export const BadRequest = new HttpError(400, "Bad Request", 1000);
 export const ApiNotFound = new HttpError(404, "Api Not Found", 1000);
 export const UserNotFound = new HttpError(404, "User Not Found", 1000);
+export const RecruitNotFound = new HttpError(404, "Recruit Not Found", 1000);
 export const IsNotAdmin = new HttpError(409, "This User is Not Admin", 1000);
