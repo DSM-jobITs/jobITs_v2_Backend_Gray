ce90cca9bc09896088b6a510f3f3d17244a755e0
yangseungbin <tanja1206@naver.com>
2021-01-06 10:04:16 +0900

    [ADD] user not found exception

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -2,4 +2,5 @@ import HttpError from "./httpError";
 
 export const BadRequest = new HttpError(400, "Bad Request", 1000);
 export const ApiNotFound = new HttpError(404, "Api Not Found", 1000);
+export const UserNotFound = new HttpError(404, "Uset Not Found", 1000);
 export const IsNotAdmin = new HttpError(409, "This User is Not Admin", 1000);
