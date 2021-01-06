0a1c46eebbceef7d219793ef3c776999ca71d966
yangseungbin <tanja1206@naver.com>
2021-01-06 16:21:53 +0900

    [ADD] craete qualification

---
+++ src/repositories/qualification.ts
@@ -0,0 +1,16 @@
+import { Qualification } from "@/entities";
+import { getRepository } from "typeorm";
+
+export class QualificationRepository {
+  public static async createQualification(
+    id: string,
+    grade: number,
+    recruitId: string
+  ) {
+    const qualification = new Qualification();
+    qualification.id = id;
+    qualification.grade = grade;
+    qualification.recruitId = recruitId;
+    await getRepository(Qualification).save(qualification);
+  }
+}
