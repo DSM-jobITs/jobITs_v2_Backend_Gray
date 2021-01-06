8f7b004ded4cce1aa1382cda4b5daf08a57adc61
yangseungbin <tanja1206@naver.com>
2021-01-06 14:48:18 +0900

    [DEL] console log

--- src/services/recruit.ts
+++ src/services/recruit.ts
@@ -4,8 +4,12 @@ import {
   ManagerRepository,
 } from "@/repositories";
 import { Recruit, Enterprise } from "@/entities";
-import { writeFirstRecruitRequest } from "@/interfaces";
+import {
+  writeFirstRecruitRequest,
+  writeSecondRecruitRequest,
+} from "@/interfaces";
 import { mkId } from "@/utils";
+import { RecruitNotFound } from "@/exception";
 
 export class RecruitService {
   public static async createRecruit(
@@ -13,11 +17,34 @@ export class RecruitService {
   ): Promise<string> {
     const uuid: string = await mkId();
     const enterprise: Enterprise = await EnterpriseRepository.createEnterprise(
-      req
+      req.name,
+      req.entNo,
+      req.phone,
+      req.sales,
+      req.sector,
+      req.establishmentDate,
+      req.address,
+      req.zipCode
     );
-    await RecruitRepository.createRecruit(req, uuid, enterprise);
-    await ManagerRepository.createManager(req);
+    console.log(enterprise);
+    await RecruitRepository.createRecruit(uuid, req.personnel, enterprise);
+    await ManagerRepository.createManager(
+      req.entNo,
+      req.managerRank,
+      req.managerPhone,
+      req.managerEmail
+    );
     return uuid;
+  }
+
+  public static async updateSecondRecruit(
+    req: writeSecondRecruitRequest,
+    id: string
+  ) {
+    const recruit = await RecruitRepository.findOneById(id);
+    if (!recruit) throw RecruitNotFound;
+    await RecruitRepository.addDetails(req.detail, id);
+    await EnterpriseRepository.addIntroduce(req.introduce, recruit.entNo);
   }
 
   public static async getWritingRecruits(): Promise<Array<Recruit>> {
