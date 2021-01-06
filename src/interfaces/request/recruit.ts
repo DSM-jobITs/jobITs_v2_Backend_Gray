8d0b886932b23691fd61eac633c9d4c72639d106
yangseungbin <tanja1206@naver.com>
2021-01-06 13:30:11 +0900

    [ADD] write first recruit request

---
+++ src/interfaces/request/recruit.ts
@@ -0,0 +1,14 @@
+export interface writeFirstRecruitRequest {
+  personnel: number;
+  name: string;
+  entNo: string;
+  phone: string;
+  sales: number;
+  sector: string;
+  establishmentDate: string;
+  address: string;
+  zipCode: string;
+  managerRank: string;
+  managerPhone: string;
+  managerEmail: string;
+}
