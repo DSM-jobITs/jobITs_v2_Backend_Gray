266f23691b189c1e58fda60007d4131d0b2319aa
yangseungbin <tanja1206@naver.com>
2021-01-06 13:31:26 +0900

    [ADD] validation check middleware

---
+++ src/middlewares/validation.ts
@@ -0,0 +1,26 @@
+import { Schema } from "joi";
+import { BadRequest } from "@/exception";
+import { Request, Response, NextFunction } from "express";
+
+export enum Parameters {
+  BODY = "body",
+  PARAM = "params",
+  QUERY = "query",
+  HEADER = "headers",
+}
+
+export default ({
+  schema,
+  parameters,
+}: {
+  schema: Schema;
+  parameters: Parameters;
+}) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(req[parameters]);
+    await schema.validateAsync({ schema, value: req[parameters] });
+    next();
+  } catch (e) {
+    next(BadRequest);
+  }
+};
