080b09427f8cd0e519670e39ff44ca476bb7173e
yangseungbin <tanja1206@naver.com>
2021-01-06 16:21:13 +0900

    [ADD] create certificate

---
+++ src/repositories/certificate.ts
@@ -0,0 +1,16 @@
+import { Certificate } from "@/entities";
+import { getRepository } from "typeorm";
+
+export class CertificateRepository {
+  public static async createCertificate(
+    id: string,
+    certificate: string,
+    qualificationId: string
+  ) {
+    const certificateEntity = new Certificate();
+    certificateEntity.id = id;
+    certificateEntity.certificate = certificate;
+    certificateEntity.qualificationId = qualificationId;
+    await getRepository(Certificate).save(certificateEntity);
+  }
+}
