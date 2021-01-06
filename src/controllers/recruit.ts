fc6cd91f4becb469b11638cf8e49bb841a5f3ea3
yangseungbin <tanja1206@naver.com>
2021-01-06 14:46:27 +0900

    [ADD] write second recruit

--- src/controllers/recruit.ts
+++ src/controllers/recruit.ts
@@ -1,15 +1,32 @@
 import { RecruitService } from "@/services";
 import { Request, Response, NextFunction } from "express";
-import { writeFirstRecruitRequest } from "@/interfaces";
+import {
+  writeFirstRecruitRequest,
+  writeSecondRecruitRequest,
+} from "@/interfaces";
 import _ from "lodash";
 
-export const writeFirstRecruit = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const id: string = await RecruitService.createRecruit(
-    req.body as writeFirstRecruitRequest
-  );
-  res.status(200).json({ id });
-};
+export class RecruitController {
+  public static async writeFirstRecruit(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const id: string = await RecruitService.createRecruit(
+      req.body as writeFirstRecruitRequest
+    );
+    res.status(200).json({ id });
+  }
+  public static async writeSecondRecruit(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const id: string = req.params.id;
+    await RecruitService.updateSecondRecruit(
+      req.body as writeSecondRecruitRequest,
+      id
+    );
+    res.status(200).end();
+  }
+}
