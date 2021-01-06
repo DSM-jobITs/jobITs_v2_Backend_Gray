28c5a5afec0b9806b079a09712b8c4cdae0a23c9
yangseungbin <tanja1206@naver.com>
2021-01-06 16:22:46 +0900

    [ADD] update third recruit

--- src/services/recruit.ts
+++ src/services/recruit.ts
@@ -2,14 +2,22 @@ import {
   RecruitRepository,
   EnterpriseRepository,
   ManagerRepository,
+  MealRepository,
 } from "@/repositories";
 import { Recruit, Enterprise } from "@/entities";
 import {
   writeFirstRecruitRequest,
   writeSecondRecruitRequest,
+  writeThirdRecruitRequest,
 } from "@/interfaces";
 import { mkId } from "@/utils";
 import { RecruitNotFound } from "@/exception";
+import {
+  QualificationRepository,
+  CertificateRepository,
+  SpecialtyRepository,
+} from "@/repositories";
+import { WelfareRepository } from "@/repositories/welfare";
 
 export class RecruitService {
   public static async createRecruit(
@@ -46,6 +54,54 @@ export class RecruitService {
     await EnterpriseRepository.addIntroduce(req.introduce, recruit.entNo);
   }
 
+  public static async updateThirdRecruit(
+    req: writeThirdRecruitRequest,
+    id: string
+  ) {
+    const recruit = await RecruitRepository.findOneById(id);
+    if (!recruit) throw RecruitNotFound;
+    await RecruitRepository.addThirdPageInfo(
+      req.startTime,
+      req.endTime,
+      req.salary,
+      req.period,
+      id
+    );
+    const qualificationId: string = await mkId();
+    await QualificationRepository.createQualification(
+      qualificationId,
+      req.grade,
+      id
+    );
+    for (let i = 0; i < req.certificates.length; i++) {
+      await CertificateRepository.createCertificate(
+        await mkId(),
+        req.certificates[i],
+        qualificationId
+      );
+    }
+    for (let i = 0; i < req.specialties.length; i++) {
+      await SpecialtyRepository.createSpecialty(
+        await mkId(),
+        req.specialties[i],
+        qualificationId
+      );
+    }
+    await MealRepository.createMeal(
+      id,
+      req.breakfast,
+      req.lunch,
+      req.dinner,
+      req.mealSalary
+    );
+    await WelfareRepository.createWelfare(
+      id,
+      req.fourMajor,
+      req.selfDevelop,
+      req.labtop,
+      req.etc
+    );
+  }
   public static async getWritingRecruits(): Promise<Array<Recruit>> {
     return await RecruitRepository.getWritingRecruit();
   }
