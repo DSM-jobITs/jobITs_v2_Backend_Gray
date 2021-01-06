5be0466776e3de50c0765af3baa6c1f7a908a071
yangseungbin <tanja1206@naver.com>
2021-01-06 16:22:14 +0900

    [ADD] create specialty

---
+++ src/repositories/specialty.ts
@@ -0,0 +1,16 @@
+import { Specialty } from "@/entities";
+import { getRepository } from "typeorm";
+
+export class SpecialtyRepository {
+  public static async createSpecialty(
+    id: string,
+    specialty: string,
+    qualificationId: string
+  ) {
+    const specialtyEntity = new Specialty();
+    specialtyEntity.id = id;
+    specialtyEntity.specialty = specialty;
+    specialtyEntity.qualificationId = qualificationId;
+    await getRepository(Specialty).save(specialtyEntity);
+  }
+}
