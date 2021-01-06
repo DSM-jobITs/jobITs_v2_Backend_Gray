915d0cc4d16a9af141128936a0033dbfc3fabe3c
yangseungbin <tanja1206@naver.com>
2021-01-06 16:22:21 +0900

    [ADD] create welfare

---
+++ src/repositories/welfare.ts
@@ -0,0 +1,20 @@
+import { Welfare } from "@/entities";
+import { getRepository } from "typeorm";
+
+export class WelfareRepository {
+  public static async createWelfare(
+    recruitId: string,
+    fourMajor: boolean,
+    selfDevelop: boolean,
+    labtop: boolean,
+    etc: string
+  ) {
+    const welfare = new Welfare();
+    welfare.recruitId = recruitId;
+    welfare.fourMajor = fourMajor;
+    welfare.selfDevelop = selfDevelop;
+    welfare.laptop = labtop;
+    welfare.etc = etc;
+    await getRepository(Welfare).save(welfare);
+  }
+}
