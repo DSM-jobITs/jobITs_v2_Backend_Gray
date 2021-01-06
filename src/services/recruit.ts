d06f4e16802c5cb3025d176bcafdcdf01ba6cd7b
yangseungbin <tanja1206@naver.com>
2021-01-06 13:34:42 +0900

    [ADD] create recruit service

--- src/services/recruit.ts
+++ src/services/recruit.ts
@@ -1,11 +1,26 @@
-import { RecruitRepository } from "@/repositories";
+import {
+  RecruitRepository,
+  EnterpriseRepository,
+  ManagerRepository,
+} from "@/repositories";
+import { Recruit, Enterprise } from "@/entities";
+import { writeFirstRecruitRequest } from "@/interfaces";
+import { mkId } from "@/utils";
 
 export class RecruitService {
-  public static async getWritingRecruitId(): Promise<string> {
-    const recruit = await RecruitRepository.getWritingRecruit();
-    if (!recruit) {
-      return (await RecruitRepository.createRecruit()).id;
-    }
-    return recruit.id;
+  public static async createRecruit(
+    req: writeFirstRecruitRequest
+  ): Promise<string> {
+    const uuid: string = await mkId();
+    const enterprise: Enterprise = await EnterpriseRepository.createEnterprise(
+      req
+    );
+    await RecruitRepository.createRecruit(req, uuid, enterprise);
+    await ManagerRepository.createManager(req);
+    return uuid;
+  }
+
+  public static async getWritingRecruits(): Promise<Array<Recruit>> {
+    return await RecruitRepository.getWritingRecruit();
   }
 }
