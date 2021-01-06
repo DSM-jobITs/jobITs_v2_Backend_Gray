2af065d979aa418651b4199f7429c91186d0bba5
yangseungbin <tanja1206@naver.com>
2021-01-06 10:05:42 +0900

    [ADD] write recruit schema

---
+++ src/schemas/recruit.ts
@@ -0,0 +1,56 @@
+import Joi from "joi";
+
+const firstRecruitSchema = Joi.object().keys({
+  personnel: Joi.number().min(0).max(9).required(),
+  name: Joi.string().required(),
+  entNo: Joi.string().length(12).required(),
+  phone: Joi.string().length(13).required(),
+  sales: Joi.number(),
+  sector: Joi.string().required(),
+  establishmentDate: Joi.string().isoDate().required(),
+  address: Joi.string().required(),
+  zipCode: Joi.string().length(5).required(),
+  managerRank: Joi.string().required(),
+  managerPhone: Joi.string().length(14).required(),
+  managerEmail: Joi.string().email().required(),
+});
+
+const secondRecruitSchema = Joi.object().keys({
+  introduce: Joi.string().length(255).required(),
+});
+
+const thirdRecruitSchema = Joi.object().keys({
+  certificates: Joi.array().items(Joi.string()),
+  grade: Joi.number().min(1).max(100).required(),
+  specialties: Joi.array().items(Joi.string()),
+  startTime: Joi.string()
+    .regex(/^([0-9]{2})\:([0-9]{2})$/)
+    .required(),
+  endTime: Joi.string()
+    .regex(/^([0-9]{2})\:([0-9]{2})$/)
+    .required(),
+  salary: Joi.number().required(),
+  period: Joi.number().required(),
+  breakfast: Joi.boolean(),
+  lunch: Joi.boolean(),
+  dinner: Joi.boolean(),
+  mealSalary: Joi.boolean(),
+  fourMajor: Joi.boolean(),
+  selfDevelop: Joi.boolean(),
+  labtop: Joi.boolean(),
+  etc: Joi.string(),
+});
+
+const fourthRecruitSchema = Joi.object().keys({
+  recruitPlan: Joi.boolean().required(),
+  reception: Joi.string().isoDate().required(),
+  deadline: Joi.string().isoDate().required(),
+  introductions: Joi.array(),
+});
+
+export {
+  firstRecruitSchema,
+  secondRecruitSchema,
+  thirdRecruitSchema,
+  fourthRecruitSchema,
+};
