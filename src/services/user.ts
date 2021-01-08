3bb34741ddc09de6a1a9da32b0d47b7ba1dd79d4
yangseungbin <tanja1206@naver.com>
2021-01-08 19:36:47 +0900

    [DEL] user find method

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,10 +1,4 @@
 import { UserNotFound } from "@/exception";
 import { UserRepository } from "@/repositories";
 
-export class UserService {
-  public static async getWritingPage(id: string): Promise<number> {
-    const user = await UserRepository.findOneById(id);
-    if (!user) throw UserNotFound;
-    return user.page;
-  }
-}
+export class UserService {}
