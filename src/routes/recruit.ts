6944987a6bc50fa619540c1c63d460b510189cb6
yangseungbin <tanja1206@naver.com>
2021-01-16 22:39:14 +0900

    [ADD] write fourth recruit

--- src/routes/recruit.ts
+++ src/routes/recruit.ts
@@ -7,6 +7,7 @@ import {
 import validate, { Parameters } from "@/middlewares/validation";
 import { RecruitController } from "@/controllers";
 import { firstRecruitSchema, thirdRecruitSchema } from "@/schemas";
+import { uploadMiddleware } from "@/middlewares/upload";
 
 const router = Router();
 
@@ -28,6 +29,14 @@ router.post(
   "/third/:id",
   authMiddleware,
   adminCheckMiddleware,
+  tryCatchMiddleware.Error(RecruitController.writeThirdRecruit)
+);
+
+router.post(
+  "/fourth/:id",
+  authMiddleware,
+  adminCheckMiddleware,
+  //uploadMiddleware.array("file", 2),
   tryCatchMiddleware.Error(RecruitController.writeThirdRecruit)
 );
 
