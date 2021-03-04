25161bac87a6e6ad22340f3a63fd816cd8a82e6c
senugbin9850 <tanja1206@naver.com>
2021-03-04 19:21:56 +0900

    [ADD] token requried exception

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -1,6 +1,7 @@
 import HttpError from "./httpError";
 
 export const BadRequest = new HttpError(400, "Bad Request", 1000);
+export const TokenRequired = new HttpError(401, "Token Required", 1000);
 export const ApiNotFound = new HttpError(404, "Api Not Found", 1000);
 export const UserNotFound = new HttpError(404, "User Not Found", 1000);
 export const RecruitNotFound = new HttpError(404, "Recruit Not Found", 1000);
