242421979eae9c0dadc7fb2cf2a638f11a934b2b
senugbin9850 <tanja1206@naver.com>
2021-03-05 16:11:07 +0900

    [FIX] next throw token required exception

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import { verify } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import config from "@/config";
+import { TokenRequired } from "@/exception";
 
 export const authMiddleware = (
   req: Request,
@@ -8,7 +9,7 @@ export const authMiddleware = (
   next: NextFunction
 ) => {
   const token: string = req.headers.authorization;
-  if (!token) res.status(401).json({ message: "token required" });
+  if (!token) next(TokenRequired);
   const bearer: string = token.split("Bearer ")[1];
   verify(bearer, config.secretKey, (err, decoded) => {
     if (err) res.status(403).json({ message: err.message });
