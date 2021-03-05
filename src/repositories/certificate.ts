a23d181b224b8fbef25cdd1a569b4c60ad1795d2
senugbin9850 <tanja1206@naver.com>
2021-03-05 16:19:39 +0900

    [ADD] create certificate

--- src/repositories/certificate.ts
+++ src/repositories/certificate.ts
@@ -1,16 +1,14 @@
 import { Certificate } from "@/entities";
-import { getRepository } from "typeorm";
+import { CertificateInsertType } from "@/interfaces";
+import { EntityRepository, getRepository } from "typeorm";
 
+@EntityRepository(Certificate)
 export class CertificateRepository {
-  public async createCertificate(
-    id: string,
-    certificate: string,
-    qualificationId: string
-  ) {
-    const certificateEntity = new Certificate();
-    certificateEntity.id = id;
-    certificateEntity.certificate = certificate;
-    certificateEntity.qualificationId = qualificationId;
-    await getRepository(Certificate).save(certificateEntity);
+  public async createCertificate(insertRequest: CertificateInsertType) {
+    await getRepository(Certificate)
+      .createQueryBuilder("enterprise")
+      .insert()
+      .into(Certificate)
+      .values(insertRequest);
   }
 }
