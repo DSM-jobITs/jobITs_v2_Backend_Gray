7df59951e3f6ce243039f8c81eab0ab4a8e14963
senugbin9850 <tanja1206@naver.com>
2021-03-05 16:27:17 +0900

    [FIX] write recruit schema

--- src/schemas/recruit.ts
+++ src/schemas/recruit.ts
@@ -1,11 +1,11 @@
 import Joi from "joi";
 
-const firstRecruitSchema = Joi.object()
+const writeRecruitSchema = Joi.object()
   .keys({
     personnel: Joi.number().min(0).max(9).required(),
     name: Joi.string().required(),
-    entNo: Joi.string().length(13).required(),
-    phone: Joi.string().length(13).required(),
+    entNo: Joi.string().length(12).required(),
+    phone: Joi.string().length(12).required(),
     sales: Joi.number(),
     sector: Joi.string().required(),
     establishmentDate: Joi.string().isoDate().required(),
@@ -14,18 +14,8 @@ const firstRecruitSchema = Joi.object()
     managerRank: Joi.string().required(),
     managerPhone: Joi.string().length(14).required(),
     managerEmail: Joi.string().email().required(),
-  })
-  .unknown();
-
-const secondRecruitSchema = Joi.object()
-  .keys({
     introduce: Joi.string().length(255).required(),
     detail: Joi.string().length(255).required(),
-  })
-  .unknown();
-
-const thirdRecruitSchema = Joi.object()
-  .keys({
     certificates: Joi.array().items(Joi.string()),
     grade: Joi.number().min(1).max(100).required(),
     specialties: Joi.array().items(Joi.string()),
@@ -45,11 +35,6 @@ const thirdRecruitSchema = Joi.object()
     selfDevelop: Joi.boolean(),
     labtop: Joi.boolean(),
     etc: Joi.string(),
-  })
-  .unknown();
-
-const fourthRecruitSchema = Joi.object()
-  .keys({
     recruitPlan: Joi.boolean().required(),
     reception: Joi.string().isoDate().required(),
     deadline: Joi.string().isoDate().required(),
@@ -57,9 +42,4 @@ const fourthRecruitSchema = Joi.object()
   })
   .unknown();
 
-export {
-  firstRecruitSchema,
-  secondRecruitSchema,
-  thirdRecruitSchema,
-  fourthRecruitSchema,
-};
+export { writeRecruitSchema };
