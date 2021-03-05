2a395f326567e9742662a2782c16448c6095758b
senugbin9850 <tanja1206@naver.com>
2021-03-05 16:24:14 +0900

    [ADD] create recruit

--- src/repositories/recruit.ts
+++ src/repositories/recruit.ts
@@ -1,6 +1,8 @@
-import { Recruit, Enterprise } from "@/entities";
-import { getRepository } from "typeorm";
+import { Recruit } from "@/entities";
+import { RecruitInsertType } from "@/interfaces";
+import { EntityRepository, getRepository } from "typeorm";
 
+@EntityRepository(Recruit)
 export class RecruitRepository {
   public async findOneById(id: string): Promise<Recruit> {
     return await getRepository(Recruit)
@@ -9,72 +11,11 @@ export class RecruitRepository {
       .getOne();
   }
 
-  public async getWritingRecruit() {
-    return await getRepository(Recruit)
-      .createQueryBuilder("recruit")
-      .where("recruit.writing = :writing", { writing: true })
-      .getMany();
-  }
-
-  public async createRecruit(
-    id: string,
-    personnel: number,
-    enterprise: Enterprise
-  ) {
-    const recruit = new Recruit();
-    recruit.id = id;
-    recruit.personnel = personnel;
-    recruit.entNo = enterprise.entNo;
-    await getRepository(Recruit).save(recruit);
-  }
-
-  public async addDetails(detail: string, id: string) {
-    return await getRepository(Recruit)
-      .createQueryBuilder("recruit")
-      .update(Recruit)
-      .set({ detail })
-      .where("id = :id", { id })
-      .execute();
-  }
-
-  public async addThirdPageInfo(
-    startTime: string,
-    endTime: string,
-    salary: number,
-    period: number,
-    id: string
-  ) {
-    return await getRepository(Recruit)
-      .createQueryBuilder("recruit")
-      .update(Recruit)
-      .set({ startTime, endTime, salary, period })
-      .where("id = :id", { id })
-      .execute();
-  }
-
-  public async addPage(id: string) {
+  public async createRecruit(insertRequest: RecruitInsertType) {
     await getRepository(Recruit)
       .createQueryBuilder("recruit")
-      .update(Recruit)
-      .set({ page: () => "page + 1" })
-      .where("id = :id", { id })
-      .execute();
-  }
-
-  public async addFinalPageInfo(
-    id: string,
-    recruitPlan: string,
-    reception: string,
-    deadline: string
-  ) {
-    await getRepository(Recruit)
-      .createQueryBuilder("recruit")
-      .update(Recruit)
-      .set({
-        recruitPlan: (recruitPlan = "true") ? true : false,
-        reception,
-        deadline,
-      })
-      .where("id = :id", { id });
+      .insert()
+      .into(Recruit)
+      .values(insertRequest);
   }
 }
