7e16ed862fcc8008906ad0f3c6b627f68d2d99b9
senugbin9850 <tanja1206@naver.com>
2021-03-07 22:33:25 +0900

    [ADD] already exists data

--- src/repositories/qualification.ts
+++ src/repositories/qualification.ts
@@ -1,15 +1,20 @@
 import { Qualification } from "@/entities";
+import { AlreadyExistsData } from "@/exception";
 import { QualificationInsertType } from "@/interfaces";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Qualification)
 export class QualificationRepository {
   public async createQualification(insertRequest: QualificationInsertType) {
-    await getRepository(Qualification)
-      .createQueryBuilder("qualification")
-      .insert()
-      .into(Qualification)
-      .values(insertRequest)
-      .execute();
+    try {
+      await getRepository(Qualification)
+        .createQueryBuilder("qualification")
+        .insert()
+        .into(Qualification)
+        .values(insertRequest)
+        .execute();
+    } catch (e) {
+      throw AlreadyExistsData;
+    }
   }
 }
