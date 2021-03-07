03bf7bfaf10e2486b4dbbb66faf436fd41f120e8
senugbin9850 <tanja1206@naver.com>
2021-03-07 22:33:32 +0900

    [ADD] already exists data

--- src/repositories/welfare.ts
+++ src/repositories/welfare.ts
@@ -1,15 +1,20 @@
 import { Welfare } from "@/entities";
+import { AlreadyExistsData } from "@/exception";
 import { WelfareInsertType } from "@/interfaces";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Welfare)
 export class WelfareRepository {
   public async createWelfare(insertRequest: WelfareInsertType) {
-    await getRepository(Welfare)
-      .createQueryBuilder("welfare")
-      .insert()
-      .into(Welfare)
-      .values(insertRequest)
-      .execute();
+    try {
+      await getRepository(Welfare)
+        .createQueryBuilder("welfare")
+        .insert()
+        .into(Welfare)
+        .values(insertRequest)
+        .execute();
+    } catch (e) {
+      throw AlreadyExistsData;
+    }
   }
 }
