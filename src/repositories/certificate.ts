c10881e893b432567d57e2be6aa0c10bc43dba8e
senugbin9850 <tanja1206@naver.com>
2021-03-07 22:33:14 +0900

    [ADD] already exists data

--- src/repositories/certificate.ts
+++ src/repositories/certificate.ts
@@ -1,15 +1,20 @@
 import { Certificate } from "@/entities";
+import { AlreadyExistsData } from "@/exception";
 import { CertificateInsertType } from "@/interfaces";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Certificate)
 export class CertificateRepository {
   public async createCertificate(insertRequest: CertificateInsertType) {
-    await getRepository(Certificate)
-      .createQueryBuilder("enterprise")
-      .insert()
-      .into(Certificate)
-      .values(insertRequest)
-      .execute();
+    try {
+      await getRepository(Certificate)
+        .createQueryBuilder("enterprise")
+        .insert()
+        .into(Certificate)
+        .values(insertRequest)
+        .execute();
+    } catch (e) {
+      throw AlreadyExistsData;
+    }
   }
 }
