ca0a521b3b1aa298b021db40998422116327fd22
senugbin9850 <tanja1206@naver.com>
2021-03-07 22:33:23 +0900

    [ADD] already exists data

--- src/repositories/meal.ts
+++ src/repositories/meal.ts
@@ -1,15 +1,20 @@
 import { Meal } from "@/entities";
+import { AlreadyExistsData } from "@/exception";
 import { MealInsertType } from "@/interfaces";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Meal)
 export class MealRepository {
   public async createMeal(insertRequest: MealInsertType) {
-    await getRepository(Meal)
-      .createQueryBuilder("qualification")
-      .insert()
-      .into(Meal)
-      .values(insertRequest)
-      .execute();
+    try {
+      await getRepository(Meal)
+        .createQueryBuilder("qualification")
+        .insert()
+        .into(Meal)
+        .values(insertRequest)
+        .execute();
+    } catch (e) {
+      throw AlreadyExistsData;
+    }
   }
 }
