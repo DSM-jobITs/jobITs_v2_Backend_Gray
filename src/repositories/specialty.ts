d43255d9e023b1669a88efd53e7334ec42e22582
senugbin9850 <tanja1206@naver.com>
2021-03-07 22:33:30 +0900

    [ADD] already exists data

--- src/repositories/specialty.ts
+++ src/repositories/specialty.ts
@@ -1,15 +1,20 @@
 import { Specialty } from "@/entities";
+import { AlreadyExistsData } from "@/exception";
 import { SpecialtyInsertType } from "@/interfaces";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Specialty)
 export class SpecialtyRepository {
   public async createSpecialty(insertRequest: SpecialtyInsertType) {
-    await getRepository(Specialty)
-      .createQueryBuilder("qualification")
-      .insert()
-      .into(Specialty)
-      .values(insertRequest)
-      .execute();
+    try {
+      await getRepository(Specialty)
+        .createQueryBuilder("qualification")
+        .insert()
+        .into(Specialty)
+        .values(insertRequest)
+        .execute();
+    } catch (e) {
+      throw AlreadyExistsData;
+    }
   }
 }
