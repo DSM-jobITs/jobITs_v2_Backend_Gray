51fe590fd541c9b95d881148e2d9d6a2d31cf85c
senugbin9850 <tanja1206@naver.com>
2021-03-07 22:33:10 +0900

    [ADD] already exists data

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -7,3 +7,4 @@ export const ApiNotFound = new HttpError(404, "Api Not Found");
 export const UserNotFound = new HttpError(404, "User Not Found");
 export const RecruitNotFound = new HttpError(404, "Recruit Not Found");
 export const IsNotAdmin = new HttpError(409, "This User is Not Admin");
+export const AlreadyExistsData = new HttpError(409, "Already Exists Data");
