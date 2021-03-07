a5a93f516e10334df04a5e0e3c5732df529b74f3
senugbin9850 <tanja1206@naver.com>
2021-03-07 22:33:21 +0900

    [ADD] already exists data

--- src/repositories/manager.ts
+++ src/repositories/manager.ts
@@ -1,15 +1,20 @@
 import { Manager } from "@/entities";
+import { AlreadyExistsData } from "@/exception";
 import { ManagerInsertType } from "@/interfaces";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Manager)
 export class ManagerRepository {
   public async createManager(insertRequest: ManagerInsertType) {
-    await getRepository(Manager)
-      .createQueryBuilder("manager")
-      .insert()
-      .into(Manager)
-      .values(insertRequest)
-      .execute();
+    try {
+      await getRepository(Manager)
+        .createQueryBuilder("manager")
+        .insert()
+        .into(Manager)
+        .values(insertRequest)
+        .execute();
+    } catch (e) {
+      throw AlreadyExistsData;
+    }
   }
 }
