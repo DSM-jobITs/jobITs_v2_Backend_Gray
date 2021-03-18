52cd55df54b60fae57ea67338306498e40dd8500
senugbin9850 <tanja1206@naver.com>
2021-03-18 11:08:25 +0900

    [DEL] specialty repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -3,7 +3,7 @@ export * from "./recruit";
 export * from "./user";
 export * from "./manager";
 export * from "./certificate";
-export * from "./specialty";
 export * from "./qualification";
 export * from "./meal";
 export * from "./welfare";
+export * from "./introuction";
