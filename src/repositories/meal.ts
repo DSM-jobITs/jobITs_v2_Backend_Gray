a003674a2adb00c215c29117a5df316d2a650f58
senugbin9850 <tanja1206@naver.com>
2021-03-24 22:02:14 +0900

    [ADD] update meal

--- src/repositories/meal.ts
+++ src/repositories/meal.ts
@@ -1,6 +1,6 @@
 import { Meal } from "@/entities";
 import { AlreadyExistsData } from "@/exception";
-import { MealInsertType } from "@/interfaces";
+import { MealInsertType, MealUpdateType } from "@/interfaces";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Meal)
@@ -16,5 +16,13 @@ export class MealRepository {
     } catch (e) {
       throw AlreadyExistsData;
     }
+  }
+  public async updateMeal(recruitId: string, updateRequest: MealUpdateType) {
+    await getRepository(Meal)
+      .createQueryBuilder("meal")
+      .update(Meal)
+      .set(updateRequest)
+      .where("recruitId = :recruitId", { recruitId })
+      .execute();
   }
 }
