29ec619e1a809f1e2c70f3bca20bbc9fb98b0de7
senugbin9850 <tanja1206@naver.com>
2021-03-24 22:01:42 +0900

    [ADD] update types

---
+++ src/interfaces/types/update.ts
@@ -0,0 +1,62 @@
+export interface EnterpriseUpdateType {
+  entNo: string;
+  name: string;
+  phone: string;
+  sales: number;
+  sector: string;
+  establishmentDate: string;
+  address: string;
+  workers: number;
+  zipCode: string;
+  introduce: string;
+}
+
+export interface RecruitUpdateType {
+  reception: string;
+  deadline: string;
+  recruitPlan: boolean;
+  startTime: string;
+  endTime: string;
+  salary: number;
+  period: number;
+  expired: boolean;
+  personnel: number;
+  detail: string;
+  entNo: string;
+}
+
+export interface CertificateUpdateType {
+  certificate: string;
+}
+
+export interface IntroductionUpdateType {
+  originalName: string;
+  fileUuid: string;
+  entNo: string;
+}
+
+export interface ManagerUpdateType {
+  entNo: string;
+  managerRank: string;
+  managerPhone: string;
+  managerEmail: string;
+}
+
+export interface MealUpdateType {
+  breakfast: boolean;
+  lunch: boolean;
+  dinner: boolean;
+  mealSalary: boolean;
+}
+
+export interface QualificationUpdateType {
+  grade: number;
+  specialty: string;
+}
+
+export interface WelfareUpdateType {
+  fourMajor: boolean;
+  selfDevelop: boolean;
+  labtop: boolean;
+  etc: string;
+}
