9a68c69aafba2ed29566cb5595b00a56d4201dbc
senugbin9850 <tanja1206@naver.com>
2021-03-25 14:42:26 +0900

    [FIX] is not admin statuscode

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -1,10 +1,10 @@
 import HttpError from "./httpError";
 
-export const BadRequest = new HttpError(400, "Bad Request");
+export const BadRequest = (message: string) => new HttpError(400, message);
 export const TokenRequired = new HttpError(401, "Token Required");
 export const InvalidToken = new HttpError(403, "Invalid Token");
 export const ApiNotFound = new HttpError(404, "Api Not Found");
 export const UserNotFound = new HttpError(404, "User Not Found");
 export const RecruitNotFound = new HttpError(404, "Recruit Not Found");
-export const IsNotAdmin = new HttpError(409, "This User is Not Admin");
 export const AlreadyExistsData = new HttpError(409, "Already Exists Data");
+export const IsNotAdmin = new HttpError(410, "This User is Not Admin");
