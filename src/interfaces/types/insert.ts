cc89bf9d498fd192d80448eaab0521dcad5220e7
senugbin9850 <tanja1206@naver.com>
2021-04-06 14:18:38 +0900

    [FIX] allowance type string

--- src/interfaces/types/insert.ts
+++ src/interfaces/types/insert.ts
@@ -19,7 +19,7 @@ export interface RecruitInsertType {
   recruitPlan: boolean;
   startTime: string;
   endTime: string;
-  allowance: number;
+  allowance: string;
   salary: number;
   period: number;
   expired: boolean;
